1b5e818dedf278c94a4ebc9905bdd107905f3c72
Hoyoung Song <hoysong@c3r2s3.42gyeongsan.kr>
2024-03-22 15:57:16 +0900

    fix: memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: hoysong <hoysong@student.42gyeongsan.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 21:09:31 by hoysong           #+#    #+#             */
-/*   Updated: 2024/03/22 11:54:15 by hoysong          ###   ########.fr       */
+/*   Updated: 2024/03/22 15:54:16 by hoysong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	ptr1 = (unsigned char *)s1;
 	ptr2 = (unsigned char *)s2;
-	while (*ptr1 == *ptr2 && n - 1 != 0)
+	while (*ptr1 == *ptr2 && n > 1)
 	{
 		ptr1++;
 		ptr2++;
