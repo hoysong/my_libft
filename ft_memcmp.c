df28d3713b8719f782b8c698625ea92bbb8a3a17
Hoyoung Song <hoysong@c3r1s6.42gyeongsan.kr>
2024-03-24 05:46:45 +0900

    fix strnstr: if(n == 0)

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: hoysong <hoysong@student.42gyeongsan.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 21:09:31 by hoysong           #+#    #+#             */
-/*   Updated: 2024/03/22 15:54:16 by hoysong          ###   ########.fr       */
+/*   Updated: 2024/03/24 05:46:09 by hoysong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	ptr1 = (unsigned char *)s1;
 	ptr2 = (unsigned char *)s2;
+	if (n == 0)
+		return (0);
 	while (*ptr1 == *ptr2 && n > 1)
 	{
 		ptr1++;
