014f033760a99851d7f044cefcdf5c5b90524257
Hoyoung Song <hoysong@c3r2s3.42gyeongsan.kr>
2024-03-25 23:16:39 +0900

    fix calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: hoysong <hoysong@42gyeongsan.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 21:05:09 by hoysong           #+#    #+#             */
-/*   Updated: 2024/03/25 20:33:01 by hoysong          ###   ########.fr       */
+/*   Updated: 2024/03/25 23:11:14 by hoysong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,13 @@ void	*ft_calloc(size_t num, size_t size)
 {
 	unsigned char	*ptr;
 	size_t			i;
+	size_t			total;
 
+	total = size * num;
+	if (num == 0 || size == 0)
+		return (malloc(0));
+	if (total / size != num)
+		return (0);
 	ptr = malloc(size * num);
 	if (ptr == 0)
 		return (0);
