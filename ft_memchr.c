7b3fc381aebd85a8650f746e0e89e5d32b8bc91b
Hoyoung Song <hoysong@c3r2s3.42gyeongsan.kr>
2024-03-25 21:05:54 +0900

    fix ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: hoysong <hoysong@student.42gyeongsan.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 20:33:25 by hoysong           #+#    #+#             */
-/*   Updated: 2024/03/22 15:43:06 by hoysong          ###   ########.fr       */
+/*   Updated: 2024/03/25 21:04:42 by hoysong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,17 @@ void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*ptr;
 	unsigned char	bigint;
+	size_t			i;
 
+	i = 0;
 	bigint = (unsigned char)c;
 	ptr = (unsigned char *)s;
-	while (*ptr != bigint && n > 1)
+	while (i < n)
 	{
+		if (*ptr == bigint)
+			return (ptr);
+		i++;
 		ptr++;
-		n--;
 	}
-	if (*ptr == bigint)
-		return ((unsigned char *)ptr);
 	return (0);
 }
